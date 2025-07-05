f9c441d4a9be9fe13605719166fdb3020955eeea
nagi4205 <nagisome1234@gmail.com>
2025-07-05 21:01:02 +0900

    4th commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,9 @@ export default function Home() {
 				<h1 className="text-4xl font-bold">under construction...</h1>
 			</main>
 			<Link href="/test">
-				<button className="bg-blue-500 text-white px-4 py-2 rounded-md">Go to test page</button>
+				<button type="button" className="bg-blue-500 text-white px-4 py-2 rounded-md">
+					Go to test page
+				</button>
 			</Link>
 		</div>
 	);
